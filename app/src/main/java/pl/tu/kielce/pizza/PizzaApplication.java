a8ee05d48a0d6de58b484deeb90407c976ec7c60
Mateusz Nalepa <mateusz.nalepa@eo.pl>
2018-03-08 14:12:08 +0100

    dodanie wersji, użycie dependencyManagment

--- app/src/main/java/pl/tu/kielce/pizza/PizzaApplication.java
+++ app/src/main/java/pl/tu/kielce/pizza/PizzaApplication.java
@@ -2,10 +2,11 @@ package pl.tu.kielce.pizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 public class PizzaApplication {
 
