e9fec9557ee59cd7009ddc9a96b0c131b84e1392
Mateusz Nalepa <mateusz.nalepa@softylabs.com>
2018-07-05 14:46:10 +0200

    remove flux testing, add whiz 33, practise test 3

---
+++ app/src/main/java/pl/tu/kielce/pizza/nauka/whiz/practise3/Whiz33.java
@@ -0,0 +1,17 @@
+package pl.tu.kielce.pizza.nauka.whiz.practise3;
+
+public class Whiz33 {
+
+    public static void main(String[] args) {
+
+        int i;
+        i = 6;
+        System.out.println(i);
+        for (int x = 10; x > 5; x++) {
+            if (x == 16)
+                x-=11;
+
+            System.out.println(x + "");
+        }
+    }
+}
